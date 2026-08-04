9a4d1e061f56691d426c51c04d5b83bdcc21766b
Dominic Duxbury <domduxbury@hotmail.com>
2026-08-04 11:41:22 +0000

    🔒️ Updated run.sh to keep password secure.

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ echo "Please enter your router admin name: "
 read name
 echo "Please enter the password for $name: "
 read -s password
-python main.py $name $password & python texter/main.py
+python main.py $name <( s=$password printenv s ) & python texter/main.py
 exit 0
